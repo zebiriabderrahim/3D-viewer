2fc55a6264e8d783cf1c637e127bde05b4ada6ad
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-03-17 01:22:52 -0400

    add basic vertexShader & fragmentShader

--- 3DViewerApp/app/fragmentShader.glsl
+++ 3DViewerApp/app/fragmentShader.glsl
@@ -1,11 +1,12 @@
-#version 330
-
+#version 410 core
 
 layout(location = 0) out vec4 fragColor;
 
+uniform vec4 u_Color;
+
 void main() {
 
-    fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    fragColor = u_Color;
 }
 
 
