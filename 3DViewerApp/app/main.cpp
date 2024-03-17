f1e38c3755bb06107fbb24b9c2a398dca563582f
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-03-17 01:22:06 -0400

    add basic main to test the abstraction

--- 3DViewerApp/app/main.cpp
+++ 3DViewerApp/app/main.cpp
@@ -3,149 +3,116 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include "spdlog/spdlog.h"
-
-#include <string>
 #include <sstream>
 #include <fstream>
+#include <string>
 
-#define PBYTE(CHAINE) ( (CHAINE) != NULL ? (CHAINE) : (const GLubyte *) "????" )
+#include "spdlog/spdlog.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "ShaderProgram.h"
+#include "Shader.h"
+#include "Renderer.h"
 
 
-static GLuint compileShader(GLuint type, const std::string& source){
-    unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if(result == GL_FALSE){
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)alloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader!" << std::endl;
-        std::cout << message << std::endl;
-        glDeleteShader(id);
-        return 0;
+#define PBYTE(CHAINE) ((CHAINE) != NULL ? (reinterpret_cast<const char*>(CHAINE)) : "????")
+
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
     }
-    return id;
-}
-static GLuint creatShader(const std::string& vertexShader, const std::string& fragmentShader){
-    unsigned int progId = glCreateProgram();
-    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
-    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
-    glAttachShader(progId, vs);
-    glAttachShader(progId, fs);
-    glLinkProgram(progId);
-    glValidateProgram(progId);
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-    return progId;
-}
-
-std::string readShader(const std::string& filepath){
-    std::ifstream stream(filepath);
-    //error handling
-    if(!stream){
-        std::cerr << "Failed to open file" << std::endl;
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        spdlog::info("Space key is pressed");
     }
-    std::string line;
-    std::stringstream ss;
-    while(getline(stream, line)){
-        ss << line << '\n';
-    }
-    return ss.str();
 }
 
 
-
-int main()
-{
-
-    if (!glfwInit())
-    {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
+int main() {
+    if (!glfwInit()) {
+        spdlog::error("Failed to initialize GLFW");
         return -1;
     }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on macOS
 #endif
 
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(640, 480, "3D Viewer", nullptr, nullptr);
-    if (!window)
-    {
-        std::cerr << "Failed to create window" << std::endl;
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "3D Viewer", nullptr, nullptr);
+    if (!window) {
+        spdlog::error("Failed to create GLFW window");
         glfwTerminate();
         return -1;
     }
 
-    // Make the window's context current
     glfwMakeContextCurrent(window);
 
-    // Initialize GLEW
-    if (glewInit() != GLEW_OK)
-    {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
+    if (glewInit() != GLEW_OK) {
+        spdlog::error("Failed to initialize GLEW");
         return -1;
     }
 
-
-
-    const GLubyte *glVersion  = glGetString( GL_VERSION );
-    const GLubyte *glVendor   = glGetString( GL_VENDOR );
-    const GLubyte *glRenderer = glGetString( GL_RENDERER );
-    const GLubyte *glslVersion = glGetString( GL_SHADING_LANGUAGE_VERSION );
-    std::cout << "// OpenGL " << PBYTE(glVersion) << PBYTE(glVendor) << std::endl;
-    std::cout << "// GPU    " << PBYTE(glRenderer) << std::endl;
-    std::cout << "// GLSL   " << PBYTE(glslVersion) << std::endl;
-
-    float positions[] = {
-        -1.0f, -0.5f,
-         0.0f,  0.5f,
-         1.0f, 0.5f
-    };
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
-
-
-//    std::string vertexShader = readShader("../vertexShader.glsl");
-//    std::string fragmentShader = readShader("../fragmentShader.glsl");
-//    unsigned int shader = creatShader(vertexShader, fragmentShader);
-//    glUseProgram(shader);
-
-
-
-
-    // Loop until the user closes the window
-    while (!glfwWindowShouldClose(window))
+    // Output OpenGL, GPU, and GLSL versions
+    spdlog::info("OpenGL {}", PBYTE(glGetString(GL_VERSION)), PBYTE(glGetString(GL_VENDOR)));
+    spdlog::info("Vendor  {}", PBYTE(glGetString(GL_VENDOR)));
+    spdlog::info("Renderer  {}", PBYTE(glGetString(GL_RENDERER)));
+    spdlog::info("GLSL  {}", PBYTE(glGetString(GL_SHADING_LANGUAGE_VERSION)));
     {
-        // Render here
-        glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        // Swap front and back buffers
-        glfwSwapBuffers(window);
+        // Vertex Buffer
+        float positions[] = {
+                -0.5f, -0.5f,
+                0.5f, -0.5f,
+                0.5f, 0.5f,
+                -0.5f, 0.5f
+        };
+        unsigned int indices[] = {
+                0, 1, 2, 2, 3, 0
+        };
 
-        // Poll for and process events
-        glfwPollEvents();
+        v3D::VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        v3D::VertexBufferLayout layout;
+        layout.addElement<float>(2);
+        v3D::VertexArray va;
+        va.addBuffer(vb, layout);
+
+        v3D::IndexBuffer ib(indices, 6);
+
+        va.unbind();
+
+        v3D::ShaderProgram shaderProgram;
+        shaderProgram.initFromFiles("../vertexShader.glsl", "../fragmentShader.glsl");
+        shaderProgram.use();
+
+        GLint location = shaderProgram.getUniformLocation("u_Color");
+
+        glUniform4f(location, 0.6f, 0.0f, 0.0f, 1.0f);
+        glClearColor(0.55f, 0.5f, 0.5f, 1.0f);
+
+        v3D::Renderer renderer;
+
+
+        // Main loop
+        while (!glfwWindowShouldClose(window)) {
+            processInput(window);
+            renderer.clear();
+            va.bind();
+            ib.bind();
+            renderer.draw(va, ib, shaderProgram);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+
+        // Cleanup
+
+        spdlog::info("Closing application");
     }
-    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-    spdlog::debug("This message should be displayed..");
-    spdlog::info("This message should be displayed..");
-
-    // change log pattern
-    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-
     glfwTerminate();
     return 0;
 }
-
